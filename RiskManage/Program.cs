cd0d2bcd0edfd29b18eba4cf471457b8c954d2ab
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-06-22 16:38:16 +0530

    Add files via upload

---
+++ RiskManage/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiskManage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TDSDataModel Tdsobj = new TDSDataModel();
+            RDSDataModel Rdsobj = new RDSDataModel();
+
+            Tdsobj.SetTradeId(1231);
+            Tdsobj.SetDate("20210104");
+            Tdsobj.SetCurrentTradeValue(214354);
+            Tdsobj.SetCounterPartyId(34154231);
+
+
+            int TradeIdValue=Tdsobj.GetTradeId();
+            String DateValue=Tdsobj.GetDate();
+            int CurrentTradeValue1=Tdsobj.GetCurrentTradeValue();
+            int CounterPartyIdValue=Tdsobj.GetCounterPartyId();
+
+
+
+        Rdsobj.SetCounterPartyId(224144354);
+            Rdsobj.SetName("thanya");
+            int CounterPartyIdValue1 = Rdsobj.GetCounterPartyId();
+            String NameValue = Rdsobj.GetName();
+
+    }
+}
+}
