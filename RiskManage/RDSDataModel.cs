7b3fcafe866fd2d81d515d3e78b692896defb725
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-06-22 14:07:25 +0530

    Add files via upload

---
+++ RiskManage/RDSDataModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiskManage
+{
+    class RDSDataModel
+    {
+        private int CounterPartyId;
+        private String Name;
+
+        public int GetCounterPartyId() { return this.CounterPartyId; }
+        public String GetName() { return this.Name; }
+
+        public void SetCounterPartyId(int value) { this.CounterPartyId = value; }
+        public void SetName(string value) { this.Name = value; }
+    }
+}
